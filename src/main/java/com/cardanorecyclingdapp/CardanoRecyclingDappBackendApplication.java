a826e8388012aa7fe5120777d6d2757220666ff7
epaikins <eugenepokooaikinsdev@gmail.com>
2022-08-04 22:21:23 +0000

    Initial Commit

---
+++ src/main/java/com/cardanorecyclingdapp/CardanoRecyclingDappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cardanorecyclingdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardanoRecyclingDappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardanoRecyclingDappBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cardanorecyclingdapp/CardanoRecyclingDappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardanorecyclingdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardanoRecyclingDappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
